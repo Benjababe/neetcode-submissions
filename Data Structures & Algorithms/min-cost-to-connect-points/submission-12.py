e10e33ed989fa9a5158f51eb9dd48d0a17fa866c
Benjababe <benjamin2471@gmail.com>
2026-09-18 20:07:00 +0800

    Add: min-cost-to-connect-points - submission-12

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-12.py
@@ -0,0 +1,45 @@
+from queue import PriorityQueue
+
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        if len(points) == 1:
+            return 0
+        
+        visited = set()
+        total_dist = 0
+
+        points = [(p[0], p[1]) for p in points]
+
+        islands = {}
+        for i in range(len(points)):
+            islands[i] = f"{i}"
+
+        pq = PriorityQueue()
+        for i in range(len(points)-1):
+            for j in range(i+1, len(points)):
+                x1, y1 = points[i]
+                x2, y2 = points[j]
+                dist = abs(x1-x2) + abs(y1-y2)
+                pq.put((dist, i, j))
+
+        edges = 0
+        while not pq.empty():
+            dist, p1, p2 = pq.get()
+
+            if islands[p1] == islands[p2]:
+                continue
+
+            to_change = islands[p2]
+            for k, v in islands.items():
+                if v == to_change:
+                    islands[k] = islands[p1]
+
+            total_dist += dist
+            visited.add(p1)
+            visited.add(p2)
+            edges += 1
+
+            if edges == len(points)-1:
+                break
+
+        return total_dist
